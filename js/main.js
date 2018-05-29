dcdf518197a6b0380f3b6d02be5cb8508e1fe91e
Kevin <GreenApple76@users.noreply.github.com>
2018-05-29 19:55:20 -0400

    Fix last character not being appended to textarea

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 $(function() {
-    var sentence = 'The quick brown fox jumps over the lazy dog';
+    // var sentence = 'The quick brown fox jumps over the lazy dog';
+    var sentence = 'The';
     var timerStarted = false;
-    var timer = 30;
+    var timer = 10;
     var charCount = 0;
     var intervalID;
 
+    $('textarea').on('focus', function() {
+        $(this).prop('placeholder', '');
+    });
+
     $('textarea').on('keydown', function(e) {
         switch (e.keyCode) {
-            case 8:  // Backspace
             case 9:  // Tab
             case 13: // Enter
             case 16: // Shift
@@ -18,8 +22,15 @@ $(function() {
             case 38: // Up
             case 39: // Right
             case 40: // Down
-            case 46: // Delete
                 break; // do not count above keys in character count
+            case 46: // Delete
+            case 8:  // Backspace
+                // remove deleted characters from character count if
+                // 1 or more characters were deleted
+                if (charCount > 0) {
+                    charCount--;
+                }
+                break;
 
             default:
                 charCount++;
@@ -64,7 +75,10 @@ $(function() {
     // timer expired or user completed typing entire sentence
     // disable typing, stop countdown timer, update timer on web page
     function endTest() {
-        $('textarea').prop('disabled', true);    
+        // wrap textarea disabling in setTimeout to provide slight
+        // delay, allowing keyup/change event(?) time to display final character
+        // in textarea before being disabled
+        setTimeout(function() {$('textarea').prop('disabled', true);}, 0);
         clearInterval(intervalID);
         $('#timer').text('0');
     }
