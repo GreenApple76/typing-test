9fbc2709a1b91dbcbbd39bc8278c49e1f72a9d92
Kevin <GreenApple76@users.noreply.github.com>
2018-05-29 15:57:47 -0400

    Add additional countdown timer comments

--- js/main.js
+++ js/main.js
@@ -6,19 +6,28 @@ $(function() {
         // start countdown timer on initial keypress
         if(!timerStarted) {
             timerStarted = true;
+
+            // reduce countdown timer by 1 second
             timer--;
+            // update timer on web page
             $('#timer').text(timer);
+            // invoke the countdown timer every second
             var intervalID = setInterval(countdownTimer, 1000);
+
             function countdownTimer() {
+                // reduce countdown timer by 1 second
                 timer--;
-                // Countdown timer has expired: disable typing, stop counting down
                 if (timer === 0) {
+                    // Countdown timer has expired: disable typing, stop counting down
                     $('textarea').prop('disabled', true);    
                     clearInterval(intervalID);
                     $('#timer').text(timer);
                 } else if (timer < 10) {
+                    // prefix times less than ten seconds with 0
+                    // update timer on web page
                     $('#timer').text('0' + timer);
                 } else {
+                    // update timer on web page
                     $('#timer').text(timer);
                 }
             }
